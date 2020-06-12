7c4d5beb5e7cf21ff3d5b2ffd041687e69a68cb6
Neil Kindlon <kindln@sumner039.sumner.jax.org>
2020-06-12 12:53:22 -0400

    Added setup for packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+# Setup file needed for packaging
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metadata_mongo)ingester",
+    version="1.1",
+    author="Neil Kindlon",
+    author_email="Neil.Kindlon@jax.org",
+    description="Ingest metadata into mongodb"
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheJacksonLaboratory/metadata_mongo_ingester", 
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
